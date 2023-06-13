d12fa7e82749402fb0465318321260d511499586
DanielDev01VLakes <132612736+DanielDev01VLakes@users.noreply.github.com>
2023-06-12 22:19:27 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var peso = Number(prompt('Digite seu peso:'));
+var altura = Number(prompt('Digite sua altura:'))
+var imc = peso / (altura * altura)
+
+console.log(peso / altura) + (altura * altura);
+console.log("Seu imc é: ", imc);
